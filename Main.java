b8c36d2dad20a90608faa2b240968ea80a8c075f
nurfaika <95457551+nurfaika@users.noreply.github.com>
2021-12-17 10:22:09 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class Main
+{
+    String player:
+    int dadu;
+    int petak;
+}
+
---
+++ dadu.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class dadu {
+    public dadu(){
+        
+    }
+public int acak(){
+    Random random = new Random();
+    int nilaidadu=0;
+    nilaidadu = random.nextInt(6);
+    return nilaidadu;
+    System.out.println("Jumlah dadu yang keluar adalah" + );
+
+    }
+}
